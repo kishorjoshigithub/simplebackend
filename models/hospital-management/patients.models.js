363296196a416586a5f079b2affa4b30b4cec2a5
Kishor Joshi <kishorjoshiabd@gmail.com>
2025-05-31 09:52:56 +0530

    model for hospital management ( incomplete )

---
+++ models/hospital-management/patients.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const PatientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    diagnosedWith: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+
+    bloodGroup: {
+      type: String,
+      required: true,
+    },
+    admittedIn: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Patient = mongoose.model("Patient", PatientSchema);
